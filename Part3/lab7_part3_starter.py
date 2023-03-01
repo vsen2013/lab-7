8ad96bbc701a6392687da27b07ef63956007f44e
ChuniHiro <hongyufu@usc.edu>
2023-02-28 17:40:48 -0800

    part3

---
+++ Part3/lab7_part3_starter.py
@@ -0,0 +1,25 @@
+
+def calculate_distances(data, centroids):
+    """
+    Step 1: Calculate the distance between each sample and each centroid
+    """
+
+    
+def make_clusters(distances):
+    """
+    Step 2: Assign each data point to it's nearest centroid
+    """
+    
+def update_clusters(clusters, data, k, iterations):
+    """
+    Step 3: Average the data points in each cluster to update
+    the centroids' locations and repeat for set number of iterations
+    """
+    
+
+def mykmeans(data, k, centroids, iterations):
+    """
+    pull everything together
+    
+    You may randomly initialze your center
+    """
